d37c8cd56d2cc80cb6319a35beb475918460d5f6
SHINMH <sky95012@gmail.com>
2022-06-06 20:28:10 +0900

    프로젝트 환경 설정
    프로젝트 환경 설정, prettier, eslint, plugin 등 의존성 설치 및 설정 파일
    추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,13 +3,18 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Blog`,
-    siteUrl: `https://www.shinmh.dev`
+    siteUrl: `https://www.shinmh.dev`,
+    description: `SHINMH's Blog`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion"]
+  plugins: [`gatsby-plugin-emotion`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images\
+  ]
 };
 
 export default config;
