130eb31ae9087ab1b44e824b8e86f947fb5efbe0
SHINMH <sky95012@gmail.com>
2022-06-08 16:56:48 +0900

    header, footer 추가 & 폰트 추가

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,6 +1,45 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import Text from 'components/Text'
+import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`
+
+const Text1 = styled.div`
+  font-size: 20px;
+  font-weight: 700;
+`
+
+// Camel Case 적용
+const Text2 = styled('div')(() => ({
+  fontSize: '15px',
+  color: 'blue',
+}))
+
+const Text3 = styled.div<{ disable: boolean }>`
+  font-size: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`
+
+const Text4 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}))
 
 type InfoPageProps = {
   data: {
@@ -23,11 +62,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <div>
-        <Text text={title} />
-        <Text text={description} />
-        <Text text={author} />
-      </div>
+      <Global styles={globalStyle} />
+      <div css={TextStyle}>{title}</div>
+      <Text3 disable={true}>{description}</Text3>
+      <Text4 disable={true}>{author}</Text4>
     </div>
   )
 }
